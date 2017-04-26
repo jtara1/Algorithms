f49a9723deccccd17ca2c6bba04640751fb86555
jtara1 <jtara@tuta.io>
2017-04-26 15:34:15 -0700

    [fix] get correct scaled vector, iter over matrix rows instead of columns when solving

--- src/gaussian_elimination/ScaledPartialPivotingGE.java
+++ src/gaussian_elimination/ScaledPartialPivotingGE.java
@@ -1,34 +1,51 @@
 package gaussian_elimination;
 
 import java.lang.Math;
+import java.util.Random;
 import java.lang.Exception;
 
 public class ScaledPartialPivotingGE {
 	private Double[][] matrix = null;
+//	private Double[] x = null;
 	
 	public static void main(String[] args) {
 		// basic test case
-		Double[] row1 = {2.0, 4., 5.};
-		Double[] row2 = {-1., 2., 3.};
-		Double[][] matrix = {row1, row2};
+//		Double[] row1 = {2.0, 4., 5.};
+//		Double[] row2 = {-1., 2., 3.};
+//		Double[][] matrix = {row1, row2};
+//		
+//		ScaledPartialPivotingGE ge = null;
+//		try {
+//			ge = new ScaledPartialPivotingGE(matrix);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		
+//		ge.printMatrix(matrix);
+//		Number[] x = ge.solve();
+//		System.out.println("-------");
+//		ge.printMatrix(matrix);
+//		System.out.println("-------");
+//		ge.printSolution(x);
+		
 		
 		ScaledPartialPivotingGE ge = null;
 		try {
-			ge = new ScaledPartialPivotingGE(matrix);
+			ge = new ScaledPartialPivotingGE(3, 3, 0., 4.);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		ge.printMatrix(matrix);
 		Number[] x = ge.solve();
 		System.out.println("-------");
-		ge.printMatrix(matrix);
+		ge.printMatrix(ge.matrix);
 		System.out.println("-------");
-		ge.printVector(x);
+		ge.printSolution(x);
 	}
 	
-	public ScaledPartialPivotingGE(int rows, int columns) {
+	
+	public ScaledPartialPivotingGE(int rows, int columns, Double rangeStart, Double rangeEnd) {
 		// create matrix of size rows x columns with random values
+		matrix = getRandomMatrix(rows, columns, rangeStart, rangeEnd);
 	}
 	
 	public ScaledPartialPivotingGE(Double[][] matrix) throws Exception {
@@ -38,59 +55,76 @@ public class ScaledPartialPivotingGE {
 			throw new Exception("invalid matrix size; requirements: m >= n - 1 for a m x n matrix");
 	}
 	
-//	public static Double[][] getRandomMatrix(int rows, int columns, Double valueRangeBegin, Double valueRangeEnd) {
-//		for (int i = 0; i < rows; i++) {
-//			Double[] row = new Double[rows];
-//			for (int j = 0; j < columns; j++) {
-//				
-//			}
-//		}
-//	}
+	public static Double[][] getRandomMatrix(int rows, int columns, Double valueRangeBegin, Double valueRangeEnd) {
+		Random random = new Random();
+		
+		Double[][] matrix = new Double[rows][];
+		// iterate over the rows then the columns & insert a random value
+		for (int i = 0; i < rows; i++) {
+			Double[] row = new Double[columns];
+			for (int j = 0; j < columns; j++) {
+				// get random value in the range [valueRangeBegin, valueRangeEnd]
+				Double randomValue = random.nextDouble();
+				randomValue *= valueRangeBegin - valueRangeEnd;
+				randomValue += valueRangeEnd;
+				
+				// debug
+				randomValue = new Double((randomValue.intValue()));
+				
+				row[j] = randomValue; 
+			}
+			matrix[i] = row;
+		}
+		
+		return matrix;
+	}
 	
 	public Number[] solve() {	
 		// the indices of the rows in the matrix
-		Integer[] matrixRowIndices = new Integer[matrix.length];
+		Integer[] indexVector = new Integer[matrix.length];
 		for (int i = 0; i < matrix.length; i++) {
-			matrixRowIndices[i] = i;
+			indexVector[i] = i;
 		}
 		
 		// largest absolute value in each row
-		Double[] rowMaxValues = new Double[matrix.length];
-		for (int i = 0; i < matrix.length; i++) {
-			int index = getMaxValueIndex(matrix[i]);
-			rowMaxValues[i] = matrix[i][index];
+		Double[] scaleVector = new Double[matrix.length];
+		for (int row = 0; row < matrix.length; row++) {
+			int index = getMaxValueIndex(matrix[row]);
+			scaleVector[row] = matrix[row][index];
 		}
 		
-		// begin iterative steps
-		for (int columnIndex = 0; columnIndex < matrix[0].length - 1; columnIndex++) {
+		// iterate over all rows using this as the column index
+		for (int columnIndex = 0; columnIndex < matrix.length; columnIndex++) {
+			printMatrix(matrix);
 			Double[] ratios = new Double[matrix.length - columnIndex];
 
 			// calculate ratios
 			for (int ratiosIndex = 0, startingIndex = columnIndex; ratiosIndex < ratios.length; ratiosIndex++, startingIndex++) {
-				Double rowLeadingCoefficient = matrix[matrixRowIndices[startingIndex]][columnIndex];
-				Double rowMaxValue = rowMaxValues[startingIndex];
+				Double rowLeadingCoefficient = matrix[indexVector[startingIndex]][columnIndex];
+				Double rowMaxValue = scaleVector[startingIndex];
 				
 				ratios[ratiosIndex] = rowLeadingCoefficient / rowMaxValue;
 			}
 			
-			// swap pivot & maxValues values to front; this is not done during the last iteration
+			// swap pivot & maxValues values towards front; this is not done during the last iteration
 			if (columnIndex != matrix[0].length - 2) {
 				int largestValueIndex = getMaxValueIndex(ratios);
-				swap(rowMaxValues, columnIndex, largestValueIndex);
-				swap(matrixRowIndices, columnIndex, largestValueIndex);
+				swap(scaleVector, columnIndex, largestValueIndex);
+				swap(indexVector, columnIndex, largestValueIndex);
 			}
 			
-			Double[] pivotRow = matrix[matrixRowIndices[columnIndex]];
+			Double[] pivotRow = matrix[indexVector[columnIndex]];
 			
-			// multiply rows below pivot to make coefficients of the column 0
+			// multiply all rows except pivot to make coefficients of the column 0
 			for (int i = columnIndex + 1; i < matrix.length; i++) {
-				Double multiplier = matrix[matrixRowIndices[i]][columnIndex] / pivotRow[columnIndex];
-				subtract(multiplier, matrix[matrixRowIndices[columnIndex]], matrix[matrixRowIndices[i]]);
+				Double multiplier = matrix[indexVector[i]][columnIndex] / pivotRow[columnIndex];
+				subtract(multiplier, matrix[indexVector[columnIndex]], matrix[indexVector[i]]);
 			}
 			
 		}
-		
-		return backSubstitute(matrixRowIndices);
+		printMatrix(matrix);
+		Double[] x = backSubstitute(indexVector);
+		return x;
 	}
 	
 	/**
@@ -171,13 +205,14 @@ public class ScaledPartialPivotingGE {
 	}
 	
 	/**
-	 * Print each element in arg matrix
+	 * Print each element in arg matrix formatted
 	 * @param matrix
 	 */
-	public void printMatrix(Object[][] matrix) {
+	public void printMatrix(Number[][] matrix) {
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix[i].length; j++) {
-				System.out.print(matrix[i][j] + " ");
+				String printStr = String.format("%10.2f ", matrix[i][j]);
+				System.out.print(printStr);
 			}
 			System.out.println();
 		}
@@ -193,5 +228,16 @@ public class ScaledPartialPivotingGE {
 			System.out.print(vector[i] + " ");
 		}
 		System.out.println();
+	}
+	
+	public void printSolution(Object[] vector) {
+		for (Integer i = 0; i < vector.length; i++) {
+			String xNumber = Integer.toString(i + 1);
+			System.out.println("x" + xNumber + " = " + vector[i]);
+		}
+	}
+	
+	public Double[][] getMatrix() {
+		return matrix;
 	}
 }
