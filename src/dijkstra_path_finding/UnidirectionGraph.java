c46bbeaa84a911c73ff54b778e007fd670baa9c0
James T <jtara@tuta.io>
2017-05-26 13:42:36 -0700

    add or rename files

---
+++ src/dijkstra_path_finding/UnidirectionGraph.java
@@ -0,0 +1,99 @@
+package dijkstra_path_finding;
+
+import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class UnidirectionGraph {
+	private Float[][] graph = null;
+	
+	private Dictionary<Integer, DataKey> indexToDataKeyDict = new Hashtable<Integer, DataKey>();
+	private Dictionary<DataKey, Data> dataKeyToDataDict = new Hashtable<DataKey, Data>();
+	
+	public UnidirectionGraph() {
+		this(new CityRoadLoader("city.dat", "road.dat"));
+//		String cityDataFileName = "city.dat";
+//		String edgeDataFileName = "road.dat";
+	}
+	
+	public UnidirectionGraph(DataLoader loader) {
+		graph = loader.getAdjMatrix();
+		indexToDataKeyDict = loader.getIndexToKeyDict();
+		dataKeyToDataDict = loader.getKeyToData();
+	}
+	
+	public Dictionary<Integer, DataKey> getIndexToDataKeyDict() {
+		return indexToDataKeyDict;
+	}
+
+	public Dictionary<DataKey, Data> getDataKeyToDataDict() {
+		return dataKeyToDataDict;
+	}
+
+	public boolean insertEdge(DataKey source, DataKey destination, float distance) {
+		int src = dataKeyToDataDict.get(source).getIndex();
+		int dest = dataKeyToDataDict.get(destination).getIndex();
+		
+		return insertEdge(src, dest, distance);
+	}
+	
+	public boolean insertEdge(int source, int destination, float distance) {
+		if (graph[source][destination] != null)
+			return false; // there was already an edge from src to dest
+		
+		graph[source][destination] = distance;
+		return true;
+	}
+	
+	public boolean removeEdge(DataKey source, DataKey destination) {
+		int src = dataKeyToDataDict.get(source).getIndex();
+		int dest = dataKeyToDataDict.get(destination).getIndex();
+		
+		return removeEdge(src, dest);
+	}
+	
+	public boolean removeEdge(int source, int destination) {
+		if (graph[source][destination] == null)
+			return false; // there was already no edge from src to dest
+		
+		graph[source][destination] = null;
+		return true;
+	}
+	
+	/**
+	 * Returns the string representing the adjacency matrix ({@link #graph})
+	 */
+	@Override
+	public String toString() {
+		String result = "";
+		for (int i = 0; i < graph.length; i++) {
+			for (int j = 0; j < graph[0].length; j++) {
+				result += graph[i][j] + " ";
+			}
+			result += "\n";
+		}
+		return result;
+	}
+	
+	public String toStringDebug() {
+		String result = toString();
+		
+		for (Enumeration<Integer> e = indexToDataKeyDict.keys(); e.hasMoreElements();) {
+			Integer key = e.nextElement();
+			result += key + ": " + indexToDataKeyDict.get(key) + "\n";
+		}
+		result += "--------";
+		
+		for (Enumeration<DataKey> e = dataKeyToDataDict.keys(); e.hasMoreElements();) {
+			DataKey key = e.nextElement();
+			result += k
+		}
+		
+	}
+}
---
+++ src/dijkstra_path_finding/UserInterface.java
@@ -0,0 +1,77 @@
+package dijkstra_path_finding;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class UserInterface {
+	Scanner scanner = new Scanner(System.in);
+	UnidirectionGraph graph = null;
+	
+	public static void main(String[] args) {
+		UserInterface ui = new UserInterface();
+		ui.start();
+	}
+	
+	public void start() {
+		graph = new UnidirectionGraph();
+		commandMenu();
+	}
+	
+	private void commandMenu() {
+		String menu = "Q Query the city information by entering the city code.\n"
+				+ "D Find the minimum distance between two cities.\n"
+				+ "I Insert a road by entering two city codes and distance.\n"
+				+ "R Remove an existing road by entering two city codes.\n"
+				+ "H Display this message.\n"
+				+ "E Exit.";
+		
+		while (true) {
+			System.out.print("Enter command: ");
+			String input = scanner.nextLine().trim().toUpperCase();
+			
+			ArrayList<String> inputValues = new ArrayList<String>();
+			for (String value : input.split(" ")) {
+				inputValues.add(value);
+			}
+			
+			switch (inputValues.get(0)) {
+			case "Q":
+				// query, get city data from dictionary, cast to City, print 
+				Data city = graph.getDataKeyToDataDict().get(new CityKey(inputValues.get(1)));
+				System.out.println((City)city);
+				break;
+			case "D":
+				// distance between two cities
+				break;
+			case "I":
+				// insert road (edge)
+				graph.insertEdge(
+						new CityKey(inputValues.get(1)), 
+						new CityKey(inputValues.get(2)), 
+						Float.parseFloat(inputValues.get(3))
+						);
+				System.out.println(graph);
+				break;
+			case "R":
+				// rm road (edge)
+				graph.removeEdge(
+						new CityKey(inputValues.get(1)), 
+						new CityKey(inputValues.get(2))
+						);
+				System.out.println(graph);
+				break;
+			case "H":
+				// display help menu
+				System.out.println(menu);
+				break;
+			case "E":
+				// exit
+				System.exit(0);
+			default:
+				// invalid input
+				System.out.println("Invalid input");
+				break;
+			}
+		}
+	}
+}
