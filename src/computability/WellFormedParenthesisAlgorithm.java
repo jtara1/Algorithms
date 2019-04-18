658908daba3cadf1a311db287c651c7aa5c20c13
James T <github.jtara1@gmail.com>
2019-04-18 00:42:09 -0700

    add: aglo for checking if input is valid well formated parenthesis

---
+++ src/computability/WellFormedParenthesisAlgorithm.java
@@ -0,0 +1,38 @@
+package computability;
+
+public class WellFormedParenthesisAlgorithm {
+    /**
+     * language = (, ), [, ]
+     * @param string
+     * @return
+     */
+    public boolean isAcceptedString(String string) {
+        int parenCount = 0;
+        int bracketCount = 0;
+        int index = 0;
+
+        for (char symbol : string.toCharArray()) {
+            if (symbol == '(') ++parenCount;
+
+            else if (symbol == ')') {
+                if (index != 0 && string.charAt(index - 1) == '[') return false; // ([)] not allowed
+                --parenCount;
+            }
+
+            else if (symbol == '[') ++bracketCount;
+
+            else if (symbol == ']') {
+                if (index != 0 && string.charAt(index - 1) == '(') return false; // [(]) // not allowed
+                --bracketCount;
+            }
+
+            else return false; // symbol not in language
+
+            if (parenCount < 0 || bracketCount < 0) return false; // ()) not allowed
+
+            ++index;
+        }
+
+        return parenCount == 0 && bracketCount == 0;
+    }
+}
